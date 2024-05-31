b201f34d1d75777b74f45cc47152c78f48dff86e
RITHIKA <129405332+Rithika054@users.noreply.github.com>
2024-05-31 11:52:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import fitz  # PyMuPDF
+from PyMuPDF import fitz  # PyMuPDF
 from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
 from deep_translator import GoogleTranslator
 from fpdf import FPDF
